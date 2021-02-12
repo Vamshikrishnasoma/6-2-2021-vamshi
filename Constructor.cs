2acfcb8b0c75335ec8e502d133b85bd4b3644066
Vamshikrishnasoma <78540896+Vamshikrishnasoma@users.noreply.github.com>
2021-02-12 13:01:16 +0530

    Add files via upload

---
+++ Constructor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopstatements
+{
+    class Product
+    {
+        int pId;
+        string pName;
+        Double pPrice;
+    public Product()
+        {
+            pId = 0;
+            pName = string.Empty;
+            pPrice = 0;
+        }
+        public Product(string pName)
+        {
+            this.pName = pName;
+        }
+        public Product(int pId)
+        {
+            this.pId = pId;
+        }
+        public Product(int pId,String pName,Double pPrice)
+        {
+            this.pId = pId;
+            this.pName = pName;
+            this.pPrice = pPrice;
+        }
+        public Product(Product p)
+        {
+            this.pId = p.pId;
+            this.pName = p.pName;
+            this.pPrice = p.pPrice;
+        }
+        public void ShowValues()
+        {
+            Console.WriteLine($"{pId}\t{pName}\t{pPrice}");
+        }
+    }
+
+    class Constructor
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product(543);
+            p.ShowValues();
+            Console.WriteLine("=====s object=======");
+            Product q = new Product(573, "lenovo", 73632);
+            q.ShowValues();
+            Product s = new Product(q);
+            s.ShowValues();
+
+        }
+    }
+}
---
+++ Moverriding.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopstatements
+{
+    class parent
+    {
+        public void Display()
+        {
+            Console.WriteLine("Hello");
+        }
+        public virtual void Show()
+        {
+            Console.WriteLine("Hello .Net");
+        }
+    }
+    class child:parent
+    {
+        public override void Show()
+        {
+            Console.WriteLine("child");
+        }
+    }
+    class Moverriding
+    {
+        static void Main(string[] args)
+        {
+            parent p = new parent();
+            p.Show();
+            p=new child();
+            p.Show();
+        }
+    }
+}
