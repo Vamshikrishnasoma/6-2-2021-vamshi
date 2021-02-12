9624ae4ce1f691dcb2d0da331c67c07624ebe3d8
Vamshikrishnasoma <78540896+Vamshikrishnasoma@users.noreply.github.com>
2021-02-12 19:33:10 +0530

    Add files via upload

---
+++ ReverseNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopstatements
+{
+    class ReverseNumber
+    {
+
+        public static int Reverse(int num)
+        {
+            int rev = 0;
+            while (num > 0)
+            {
+                int digit = num % 10;
+                rev = rev * 10 + digit;
+                num = num / 10;
+            }
+            return rev;
+        }
+        public static int Sum(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Sum(9,6));
+            Console.WriteLine(Reverse(736));
+            Console.WriteLine(Reverse(456));
+        }
+    }
+}
