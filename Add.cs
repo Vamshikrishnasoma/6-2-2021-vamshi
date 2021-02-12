4a40d2bcc04a807f327564f02ad2973a1933d611
Vamshikrishnasoma <78540896+Vamshikrishnasoma@users.noreply.github.com>
2021-02-12 19:36:55 +0530

    Add files via upload

---
+++ Add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopstatements
+{
+    class Add
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseNumber.Reverse(862));
+            Console.WriteLine(ReverseNumber.Sum(29, 26));
+        }
+    }
+}
