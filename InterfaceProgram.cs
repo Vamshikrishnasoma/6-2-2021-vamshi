5b44107ebb06bcc5625f2a9abd27592c97dae509
Vamshikrishnasoma <78540896+Vamshikrishnasoma@users.noreply.github.com>
2021-02-13 12:15:02 +0530

    Add files via upload

---
+++ InterfaceProgram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopstatements
+{
+    interface Mobile
+        {
+        void price();
+        }
+    class samsung : Mobile
+    {
+        public void price()
+        {
+            Console.WriteLine("samsung price is 21000");
+        }
+    }
+        class nokia : Mobile
+        {
+            public void price()
+            {
+                Console.WriteLine("Nokia price is 12000A");
+            }
+        }
+    class InterfaceProgram
+    {
+        static void Main(string[] args)
+        {
+            Mobile s = new samsung();
+            s.price();
+            Mobile n = new nokia();
+            n.price();
+        }
+    }
+}
---
+++ InterfaceProgram1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopstatements
+{
+    class addition
+    {
+        public void add(int num1,int num2)
+        {
+            Console.WriteLine(num1+num2);
+        }
+    }
+    interface substraction
+    {
+        void sub(int num1, int num2);
+    }
+    interface Multiplication
+    {
+        void mul(int num1, int num2);
+     }
+    class Impliment:addition,substraction,Multiplication
+    {
+        public void sub(int num1,int num2)
+        {
+            Console.WriteLine(num1-num2);
+        }
+        public void  mul(int num1,int num2)
+        {
+            Console.WriteLine(num1*num2);
+        }
+    }
+    class InterfaceProgram1
+    {
+        static void Main(string[] args)
+        {
+            Impliment i = new Impliment();
+            i.sub(9, 6);
+            i.add(9, 6);
+            i.mul(9, 6);
+        }
+
+    }
+}
